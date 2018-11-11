31c25140bb09003e245afbaf6251555c82f6aebd
A dude <root@pi3.bluetears.org>
2018-11-11 17:04:06 +0100

    Test non conclusivi su presence wifi

--- shell/iPhoneD.sh
+++ shell/iPhoneD.sh
@@ -7,16 +7,13 @@ for i in `echo $DEVICES`; do
     # Change dev and eth0 if needed
     ip neigh flush dev eth0 $i
     hping3 -2 -c 10 -p 5353 -i u1 $i -q >/dev/null 2>&1
-    sleep 1
+    sleep 0.5
     # Only arp specific device, grep for a mac-address
-    status=`arp -an $i | awk '{print $4}' | grep "..:..:..:..:..:.."`
+    status=`arp -an $i | awk '{print $4}' | grep "..:..:..:..:..:.." |wc -l`
     statusMessage="OFF"
     #A mac will be 17 characters including the ":"
-    if [ ${#status} -eq 17 ]; then
-        echo "Phone $i is detected!"
+    if [ ${status} -eq 1 ]; then
         statusMessage="ON"
-    else
-        echo "Phone $i is not present"
-        statusMessage="OFF"
     fi
 done
+echo $statusMessage
