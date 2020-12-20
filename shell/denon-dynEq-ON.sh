6905be4ea5aa07a1e555b96f9746abf1116a4979
root <root@pi3.bluetears.org>
2020-12-20 13:47:11 +0100

    Sveglia e Volume Dinamico

---
+++ shell/denon-dynEq-ON.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -k 'https://172.27.72.145:10443/ajax/audio/set_config?type=9&data=%3CDynamicVolume%3E2%3C%2FDynamicVolume%3E&_=1608457236057' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:83.0) Gecko/20100101 Firefox/83.0' -H 'Accept: text/plain, */*; q=0.01' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'X-Requested-With: XMLHttpRequest' -H 'DNT: 1' -H 'Connection: keep-alive' -H 'Referer: https://172.27.72.145:10443/audio/audio.html'
