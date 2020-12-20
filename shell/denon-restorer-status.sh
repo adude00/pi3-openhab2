b092984ce5bd1674fd63d19f3cb7bb939a8a30a9
root <root@pi3.bluetears.org>
2020-12-20 14:07:03 +0100

    Restorer funzionante

---
+++ shell/denon-restorer-status.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+RESP=$(curl -s -k 'https://172.27.72.145:10443/ajax/audio/get_config?type=5&_=1608469018740' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:83.0) Gecko/20100101 Firefox/83.0' -H 'Accept: text/plain, */*; q=0.01' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'X-Requested-With: XMLHttpRequest' -H 'DNT: 1' -H 'Connection: keep-alive' -H 'Referer: https://172.27.72.145:10443/audio/audio.html' | grep -o "Restorer.*" | cut -c10-10)
+
+if [ "$RESP" = "1" ] 
+	then
+		echo OFF
+	fi
+
+if [ "$RESP" = "3" ] 
+	then
+		echo ON
+	fi
