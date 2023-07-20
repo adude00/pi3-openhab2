6c5281e8e3e11d0c0636fa4a8c1ddde8d4de992c
root <root@pi3.bluetears.org>
2023-07-20 15:55:00 +0200

    Shelly Plus Management

---
+++ transform/shellyplus_energy_kwh.js
@@ -0,0 +1,8 @@
+var logger = Java.type("org.slf4j.LoggerFactory").getLogger("shellyplus_energy_kwh.js");
+
+(function(i){
+      //logger.warn(i);
+          x = (i/1000).toFixed(2);
+	      //logger.warn(x);
+	          return x
+})(input)
