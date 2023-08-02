eae9b945dace808a61db65ce609ddb2d8a0ad6df
root <root@pi3.bluetears.org>
2023-08-02 09:25:54 +0200

    Whitenoise 60min

--- scripts/whitenoise-bdrm.sh
+++ scripts/whitenoise-bdrm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pulseaudio --start
 bluetoothctl connect DC:54:D7:03:FF:C9
-mpg123 --loop -1 -q /opt/music/07-PinkNoise-dd-128kb.mp3
+mpg123 --loop -1 -q /opt/music/07-PinkNoise-60min.mp3
