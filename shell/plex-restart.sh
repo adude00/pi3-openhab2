e06ded5bdd8dedfa33d7b4b20c7d0b371a007f83
root <root@pi3.bluetears.org>
2024-12-03 10:18:14 +0100

    BLE Sensor 0.1

--- shell/plex-restart.sh
+++ shell/plex-restart.sh
@@ -1,3 +1,6 @@
-ssh root@opti midclt call chart.release.scale plex \'{\"replica_count\": 0}\'
-sleep 10
-ssh root@opti midclt call chart.release.scale plex \'{\"replica_count\": 1}\'
+########################################################
+# DISABLED - Aggiornamento a freenas 24.10 con docker
+
+#ssh root@opti midclt call chart.release.scale plex \'{\"replica_count\": 0}\'
+#sleep 10
+#ssh root@opti midclt call chart.release.scale plex \'{\"replica_count\": 1}\'
