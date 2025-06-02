a138ef34f13ac0025976cfdc2928aa4d6efb7c63
root <root@pi3.bluetears.org>
2025-06-02 11:49:58 +0200

    Timer luce

---
+++ scripts/timer-off.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# SET
+/usr/bin/curl -s -X POST -d '{"id":1,"method":"Switch.SetConfig","params":{"id":0,"config":{"auto_off":false}}}' http://172.27.72.38/rpc |jq
+
+# STATUS
+/usr/bin/curl -s -X POST -d '{"id":0, "method":"Switch.GetConfig", "params": { "id":0 }} }' http://172.27.72.38/rpc |jq
